7b4bb347bef293e298ffc4a120bd283e3913ec6f
Zachary Tan <zdltan20@bu.edu>
2024-05-02 08:28:19 -0400

    disabled logging

--- app/camera.py
+++ app/camera.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import logging
+#import logging
 from pathlib import Path
 import sys
 import cv2
@@ -12,7 +12,7 @@ import json
 import subprocess
 
 # Set up logging
-logging.basicConfig(filename='camera_logs.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+#logging.basicConfig(filename='camera_logs.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 nnBlobPath = str((Path(__file__).parent / Path('VIADFinal_V4_openvino_2022.1_5shave.blob')).resolve().absolute())
 
@@ -36,21 +36,22 @@ syncNN = True
 # System Log Info
 def printSystemInformation(info):
     m = 1024 * 1024 # MiB
-    logging.info(f"Ddr used / total - {info.ddrMemoryUsage.used / m:.2f} / {info.ddrMemoryUsage.total / m:.2f} MiB")
-    logging.info(f"Cmx used / total - {info.cmxMemoryUsage.used / m:.2f} / {info.cmxMemoryUsage.total / m:.2f} MiB")
-    logging.info(f"LeonCss heap used / total - {info.leonCssMemoryUsage.used / m:.2f} / {info.leonCssMemoryUsage.total / m:.2f} MiB")
-    logging.info(f"LeonMss heap used / total - {info.leonMssMemoryUsage.used / m:.2f} / {info.leonMssMemoryUsage.total / m:.2f} MiB")
+    #logging.info(f"Ddr used / total - {info.ddrMemoryUsage.used / m:.2f} / {info.ddrMemoryUsage.total / m:.2f} MiB")
+    #logging.info(f"Cmx used / total - {info.cmxMemoryUsage.used / m:.2f} / {info.cmxMemoryUsage.total / m:.2f} MiB")
+    #logging.info(f"LeonCss heap used / total - {info.leonCssMemoryUsage.used / m:.2f} / {info.leonCssMemoryUsage.total / m:.2f} MiB")
+    #logging.info(f"LeonMss heap used / total - {info.leonMssMemoryUsage.used / m:.2f} / {info.leonMssMemoryUsage.total / m:.2f} MiB")
     t = info.chipTemperature
-    logging.info(f"Chip temperature - average: {t.average:.2f}, css: {t.css:.2f}, mss: {t.mss:.2f}, upa: {t.upa:.2f}, dss: {t.dss:.2f}")
-    logging.info(f"Cpu usage - Leon CSS: {info.leonCssCpuUsage.average * 100:.2f}%, Leon MSS: {info.leonMssCpuUsage.average * 100:.2f} %")
-    logging.info("----------------------------------------")
+    #logging.info(f"Chip temperature - average: {t.average:.2f}, css: {t.css:.2f}, mss: {t.mss:.2f}, upa: {t.upa:.2f}, dss: {t.dss:.2f}")
+    #logging.info(f"Cpu usage - Leon CSS: {info.leonCssCpuUsage.average * 100:.2f}%, Leon MSS: {info.leonMssCpuUsage.average * 100:.2f} %")
+    #logging.info("----------------------------------------")
 
 def is_display_connected():
     try:
         result = subprocess.run(['vcgencmd', 'get_display_power'], capture_output=True, text=True)
         return 'display_power=1' in result.stdout
     except Exception as e:
-        logging.error("Error checking display status:", e)
+        #logging.error("Error checking display status:", e)
+        print(e)
         return False
 
 # Initialize camera and pipeline
@@ -134,11 +135,13 @@ def initialize_camera():
 
 # MQTT Initialize
 def onConnect(client, userdata, flags, rc):
-    logging.info('Connected to MQTT broker')
+    #logging.info('Connected to MQTT broker')
+    print('Connected to MQTT broker')
 
 
 def onPublish(client, userdata, mid):
-    logging.info('Published MQTT message: %s', mid)
+    #logging.info('Published MQTT message: %s', mid)
+    print('Published MQTT message: %s', mid)
 
 
 client = mqtt.Client()
@@ -156,8 +159,10 @@ while True:
             break
 
     except Exception as e: # If camera not connected try again
-        logging.error("Failed to connect to camera: %s", e)
-        logging.info("Retrying in 5 seconds...")
+        #logging.error("Failed to connect to camera: %s", e)
+        #logging.info("Retrying in 5 seconds...")
+        print("Failed to connect to camera: %s", e)
+        print("Retrying in 5 seconds...")
         client.publish('tts', json.dumps({'message': f'Failed to connect to camera: {e}\nRetrying in 5 seconds...'}))
         time.sleep(5)
 
@@ -261,7 +266,7 @@ with dai.Device(pipeline) as device:
         # Send MQTT Frame Message
         numDetections = len(detectionMessages)
         if numDetections > 0:
-            logging.info('There were %d detections in this message at time %s', numDetections, time.time())
+            #logging.info('There were %d detections in this message at time %s', numDetections, time.time())
             client.publish('detections', str(detectionMessages))
 
         if not sysInfo == None:
